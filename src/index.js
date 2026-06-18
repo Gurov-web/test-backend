5803041fda9df65d83a23d96db0f361f9e2c4343
Egor Gurov <codegurov@gmail.com>
2026-06-18 22:03:53 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: ["https://test-frontend-murex-omega.vercel.app/", "http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
